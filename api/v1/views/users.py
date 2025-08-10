17372a9cddd593f9a06c70f3124729a450b2423e
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-08-10 21:00:13 +0100

    Overhaul

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -37,34 +37,31 @@ def delete_user(user_id):
     return jsonify({})
 
 
-@app_views.route('/users', methods=['POST'])
-def create_user():
-    """creates a new user"""
-    try:
-        data = request.get_json()
-    except Exception:
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def post_user():
+    """create a new user"""
+    if not request.get_json():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    if 'email' not in data.keys():
+    if 'email' not in request.get_json():
         return make_response(jsonify({'error': 'Missing email'}), 400)
-    if 'password' not in data.keys():
+    if 'password' not in request.get_json():
         return make_response(jsonify({'error': 'Missing password'}), 400)
-    new_user = User(**data)
-    new_user.save()
-    return make_response(jsonify(new_user.to_dict()), 201)
+    user = User(**request.get_json())
+    user.save()
+    return make_response(jsonify(user.to_dict()), 201)ld = storage.get("User", user_id)
 
 
-@app_views.route('/users/<user_id>', methods=['PUT'])
-def update_users(user_id):
-    """update the information inside a user object"""
-    old = storage.get("User", user_id)
-    if old is None:
+@app_views.route('/users/<string:user_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_user(user_id):
+    """update a user"""
+    user = storage.get("User", user_id)
+    if user is None:
         abort(404)
-    try:
-        data = request.get_json()
-    except Exception:
+    if not request.get_json():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    for key, value in data.items():
-        if key not in ['id', 'created_at', 'updated_at']:
-            setattr(old, key, value)
-    old.save()
-    return make_response(jsonify(old.to_dict()), 200)
+    for attr, val in request.get_json().items():
+        if attr not in ['id', 'email', 'created_at', 'updated_at']:
+            setattr(user, attr, val)
+    user.save()
+    return jsonify(user.to_dict())
